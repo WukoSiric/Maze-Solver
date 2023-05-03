1ac9c8f80c536f941b458fdd0a1af8b0bb5fadb9
WukoSiric <lstainer2002@gmail.com>
2023-05-03 23:28:25 +0930

    Started creating maze backtracking

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,17 @@
         return;
     }
 
+    function create_maze(grid: Array<[number, number]>, current_size: number, connections: Stack<[number, number]>) {
+
+    }
+
+    function check_adjacent(grid: Array<[number, number]>, position: [number, number]): Array<[number, number]> {
+        let adjacent_cords = new Array<[number, number]>(); 
+
+
+        return adjacent_cords; 
+    } 
+
 </script>
 
 <h1>Welcome to my Maze Solver! </h1>
