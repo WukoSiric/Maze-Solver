5929dc780910ca3beba128e923fb2da762537e0a
WukoSiric <lstainer2002@gmail.com>
2023-05-05 01:17:21 +0930

    Added recursive backtracking maze generation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,68 @@
 <script lang="ts">
-    import Stack from './data_structures/stack';
-
-    function test_stack() {
-        let temp_stack = new Stack<[number, number]>();
-        temp_stack.push([1, 2]);
-        temp_stack.push([3, 4]);
-        temp_stack.push([5, 6]);
-        console.log(temp_stack.pop());
-        console.log(temp_stack.pop());
-        console.log(temp_stack.pop());
-        console.log(temp_stack.pop());
-        return;
+    import type Stack from './data_structures/stack';
+    
+    // Creating maze via Recursive Backtrackign Algorithm
+    function create_maze(grid_size: number, path: Stack<[number, number]>, visited: Set<[number, number]>): Set<[number, number]> {
+        let graph = new Set<[number, number]>()
+        
+        // If the visited set contains half the dimension size of the array, return
+        if (visited.size === grid_size) {
+            return new Set<[number,number]>();
+        }
+        
+        // Get the current position from top of the stack
+        let current_position = path.pop();
+        
+        // Find the available neighbors
+        let neighbors = new Set<[number, number]>(); 
+        
+        // Check neighbor to the left
+        if (current_position![0] > 0) {
+            neighbors.add([current_position![0] -1, current_position![1]]);
+        }
+        // Check neighbor to the right
+        if (current_position![0] < grid_size - 1) {
+            neighbors.add([current_position![0] + 1, current_position![1]]);
+        }
+        // Check neighbor above
+        if (current_position![1] > 0) {
+            neighbors.add([current_position![0], current_position![1] - 1]);
+        }
+        // Check neighbor below
+        if (current_position![1] < grid_size - 1) {
+            neighbors.add([current_position![0], current_position![1] + 1]);
+        }
+        
+        // Finding unexplored neighbors
+        let unexplored_neighbors = new Array<[number, number]>();
+        for (let neighbor of neighbors) {
+            if (!visited.has(neighbor)) {
+                unexplored_neighbors.push(neighbor);
+            }
+        }
+        
+        // Return if no unexplored neighbors
+        if (unexplored_neighbors.length === 0) {
+            return graph;
+        }
+        
+        // Choosing random explored neighbor 
+        let random_neighbor = unexplored_neighbors[Math.floor(Math.random() * unexplored_neighbors.length)]; 
+        
+        // Adding to path
+        path.push(random_neighbor); 
+        
+        // Adding it to visited
+        visited.add(random_neighbor); 
+        
+        // Recursive call
+        let subgraph = create_maze(grid_size, path, visited); 
+        
+        // Add subgraph edges to graph 
+        subgraph.forEach(edge => graph.add(edge)); 
+        
+        return graph;
     }
-
-    function create_maze(grid: Array<[number, number]>, current_size: number, connections: Stack<[number, number]>) {
-
-    }
-
-    function check_adjacent(grid: Array<[number, number]>, position: [number, number]): Array<[number, number]> {
-        let adjacent_cords = new Array<[number, number]>(); 
-
-
-        return adjacent_cords; 
-    } 
-
 </script>
 
-<h1>Welcome to my Maze Solver! </h1>
-<button on:click={test_stack}> Test the stack type! </button>
+<h1> Welcome to my maze generator! </h1>
