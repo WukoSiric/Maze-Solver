1a3ca59209d8dbbd2b9e199814c3f5a443d13d0f
WukoSiric <lstainer2002@gmail.com>
2023-08-21 00:53:55 +0930

    Blank starting page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,4 @@
 <script lang="ts">
-    import Stack from './data_structures/stack';
 </script>
 
-<h1> Welcome to my maze generator! </h1>
 
-<p> This is a maze generator that uses the Recursive Backtracking Algorithm to generate a maze. </p>
